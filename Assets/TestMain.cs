960f402080e736f343627f44ac8c2da44fa04bf2
zxlii <lzx7421@hotmail.com>
2019-12-04 10:39:39 +0800

    first commit

---
+++ Assets/TestMain.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class TestMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var luaEnv = new LuaEnv();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
